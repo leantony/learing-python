1279f9abe767a9338efcc265566aca69d5043f2f
leantony <chachaantony@gmail.com>
2017-09-28 21:48:03 +0300

    initial commit

---
+++ crawler.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import urllib.request
+import re
+
+url = 'https://' + input("please enter a https url (without https://). E.g www.twitter.com\n")
+
+print("===============================================================")
+print("scanning {0} for links. This might take a while....".format(url))
+try:
+  with urllib.request.urlopen(url) as response:
+  	html = response.read().decode('utf-8')
+  	print("site content length is {0}".format(len(html)))
+  	links = re.findall('"((http|ftp)s?://.*?)"', html)
+  	print("=============================================================")
+  	for link in links:
+  		print(str(link[0]) + ',')
+  	print("=========================================================")
+  	print("successfully printed {0} links from {1}".format(len(links), url))
+except urllib.error.URLError as e:
+	print(e.reason)
