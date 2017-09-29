3030860c43f35ee6cd2b431add98045c3fc6e626
leantony <chachaantony@gmail.com>
2017-09-29 20:17:11 +0300

    cast response to string, instead of decode

--- crawler.py
+++ crawler.py
@@ -9,13 +9,13 @@ print("===============================================================")
 print("scanning {0} for links. This might take a while....".format(url))
 try:
   with urllib.request.urlopen(url) as response:
-  	html = response.read().decode('utf-8')
-  	print("site content length is {0}".format(len(html)))
-  	links = re.findall('"((http|ftp)s?://.*?)"', html)
-  	print("=============================================================")
-  	for link in links:
-  		print(str(link[0]) + ',')
-  	print("=========================================================")
-  	print("successfully printed {0} links from {1}".format(len(links), url))
+      html = str (response.read())
+      print("site content length is {0}".format(len(html)))
+      links = re.findall('"((http|ftp)s?://.*?)"', html)
+      print("=============================================================")
+      for link in links:
+          print(str(link[0]) + ',')
+      print("=========================================================")
+      print("successfully printed {0} links from {1}".format(len(links), url))
 except urllib.error.URLError as e:
-	print(e.reason)
+    print(e.reason)
