6305771c25811635e759b346f3e9558c868b1a26
leantony <chachaantony@gmail.com>
2017-11-14 00:18:18 +0300

    finalize lab 3

--- a1.py
+++ a1.py
@@ -1,52 +1,64 @@
 #! /usr/bin/env python3
 
 import urllib.request
-from operator import itemgetter
-from collections import OrderedDict
+import operator
 import re
-import matplotlib.pyplot as plotlib
+import matplotlib.pyplot as plt
 
-url = 'https://' + input("please enter a https url (without https://). E.g www.twitter.com\n")
+url = input("please enter a https url (without https://). E.g www.twitter.com\n")
+if url is "": # use a default url
+  url = 'https://' + 'www.twitter.com'
+else:
+  url = 'https://' + url
 
 print("===============================================================")
 print("scanning {0} for links. This might take a while....".format(url))
 
-items = set()
 scanned = []
-words_to_ignore = ["the", "at", "&nbsp", "n", ".js"]
-words_scanned = []
-scanned_appearances = {}
+url_regexp = '"((http|ftp)s?://.*?)"'
+p_tags_regexp = '<p>(.*?)<\/p>'
+words_to_ignore = [" ", "target=\"_blank\"", "[", "]", '\\\\n']
+words_scanned = {}
 statistics = {}
-def scan(url, depth=0, depth_max=10):
+#
+def scan(url, items = set(), depth=0, depth_max=3):
   try:
     with urllib.request.urlopen(url) as response:
         html = str(response.read())
         print("site content length is {0}".format(len(html)))
-        marked = 0
         # words
-        words = re.compile(r'<[^>]+>').sub('', html) # ignore html tags
-        stripped_words = re.findall(r'\w+', words) # fetch words only
-        for w in stripped_words:
-          if w in words_to_ignore:
+        pattern = re.compile(p_tags_regexp, re.DOTALL) # use paragraphs as instructed
+        words = pattern.findall(html)
+        
+        for w in str(words).split(' '):
+          if w in words_to_ignore or len(w) < 2:
             # ignore set words
             continue
           else:
             if w in words_scanned:
-              scanned_appearances[w] = c = marked = marked + 1 # how namy times the word appears
-              statistics[url] = scanned_appearances
+              if re.compile(r'[^\w\.@-]').match(w): # skip invalid chars
+                continue
+              else:
+                apps = words_scanned[w] # how many times it intially appeared
+                words_scanned[w] = apps + 1
+                statistics[url] = words_scanned
             else:
-              words_scanned.append(w)
-        links = re.findall('"((http|ftp)s?://.*?)"', html)
+              words_scanned[w] = 1 # intial appearances
+      
+        links = re.findall(url_regexp, str(words)) # find the links in the paragraphs identified above
         for link in links:
-            items.add(link[0]) # add the link to the list
+            # the link is a tuple, so we access the first item, which is the link
+            items.add(link[0])
         print("successfully printed {0} links from {1}".format(len(links), url))
-        for x in items:
+        for x in items.copy():
           if depth == depth_max:
             print("depth reached.")
-            # print(scanned)
-            # print(words_scanned)
-            plotlib.scatter(range(1, len(statistics)), statistics.items())
-            plotlib.show()
+            sorted_values = sorted(words_scanned.items(), key=operator.itemgetter(1))
+            sorted_keys = sorted(words_scanned.items(), key=operator.itemgetter(0))
+            # print(sorted_keys)
+            # print(sorted_values)
+            plt.plot(sorted_keys[:5], sorted_values[:5])
+            plt.show()
 
             exit(0)
           else:
@@ -55,30 +67,14 @@ def scan(url, depth=0, depth_max=10):
               continue
             else:
               scanned.append(x)
-              scan(x, depth=depth+1)
+              scan(x, items, depth=depth+1)
   except urllib.error.URLError as e:
       print(e.reason)
 
 
-scan(url);
-
-graph = {url: set(items)};
-
-# print(graph);
-# def dfs(graph, start, visited=None, d_max=10):
-#     depth=0
-#     if visited is None:
-#         visited = set()
-#     visited.add(start)
-#     for next in graph[start] - visited:
-#         if depth >= d_max:
-#           print("exhausted depths...");
-#           break;
-#         else:
-#           depth+=1;
-#           dfs(graph, next, visited)
-#     return visited
-
-
-# result = dfs(graph, 'A');
-# print(result);
+def perform_work():
+  time_start = time.time()
+  scan(url);
+  time_end = time.time()
+  print("==========================================================")
+  print('task finished in {} ms'.format(int(time_end - time_start * 1000)))
